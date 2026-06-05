0a319a85346e68d0e000fb5c6b0a8f368e709b09
Konke <ST10481607@rcconnect.edu.za>
2026-06-05 20:35:52 +0200

    Add user memory fields and comments

--- CyberSecurityChatbotGUI/User.cs
+++ CyberSecurityChatbotGUI/User.cs
@@ -3,6 +3,7 @@
     internal class User
     {
         public string Name { get; set; }
+        // Stores user name and favourite topic for memory feature
         public string FavoriteTopic { get; set; }
         public string LastTopic { get; set; }
         public string DetectedSentiment { get; set; }
